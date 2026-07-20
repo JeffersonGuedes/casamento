2deef934a56432aa3174d58d9ae6f0cdb0dac090
jefferson guedes <jeffersonguedes0211@gmail.com>
2026-07-19 23:33:10 -0300

    :snake: Feat: Adicionando outro campo para preencher confirmação de presença

--- rsvp/models.py
+++ rsvp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Guest(models.Model):
     name = models.CharField(max_length=255)
+    name_companions = models.CharField(max_length=100, blank=True)
     phone = models.CharField(max_length=20, blank=True)
     is_attending = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
