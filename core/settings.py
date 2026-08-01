8d2c705d35cb305691b638a842ad82a87e6eaced
jefferson guedes <jeffersonguedes0211@gmail.com>
2026-08-01 18:49:08 -0300

    :snake: Feat: config dockerfile

--- core/settings.py
+++ core/settings.py
@@ -45,10 +45,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    'https://lavinia.jeffersonguedes.com.br',
+]
 
 ROOT_URLCONF = 'core.urls'
-
 
 
 TEMPLATES = [
